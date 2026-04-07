896f6e4b13746e27301db0509c2b7f1b3bc2b469
hanakhalda11-del <hanakhalda11@gmail.com>
2026-04-07 15:37:37 +0700

    Add files via upload

---
+++ structinstruct.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+struct detailalamat
+{
+    string desa;
+    string kota;
+};
+
+struct orang 
+{
+    string nama;
+    detailalamat alamat;
+    int umur;
+};
+
+int main()
+{
+// deklarasi object dari struct orang
+    orang mhs;
+// isi object
+    cout << "nama mahasiswa : ";
+    cin >> mhs.nama;
+    cout << "alamat desa : ";
+    cin >> mhs.alamat.desa;
+    cout << "alamat kota : ";
+    cin >> mhs.alamat.kota;
+    cout << "umur : ";
+    cin >> mhs.umur;
+    cout << endl;
+// menampilkan isi object
+    cout << "nama : " << mhs.nama <<endl;
+    cout << "desa : " << mhs.alamat.desa <<endl;
+    cout << "kota : " << mhs.alamat.kota <<endl;
+    cout << "umur : " << mhs.umur <<endl;
+}
