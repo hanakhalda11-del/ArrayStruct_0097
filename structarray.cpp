a18976edd8d233d2597b779d61ce91c5ad2ac99a
hanakhalda11-del <hanakhalda11@gmail.com>
2026-04-07 15:38:24 +0700

    Add files via upload

---
+++ structarray.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+struct detailalamat
+{
+    string desa;
+    string kota;
+};
+
+struct orang 
+{
+    string nama;
+    detailalamat alamat;
+    int umur;
+};
+
+int main()
+{
+// deklarasi object dari struct orang
+    orang mhs[3];
+// isi object
+    for (int i = 0; i <= 2; i++)
+    {
+        cout << "nama mahasiswa : ";
+        getline (cin, mhs[i].nama);
+        cout << "alamat desa : ";
+        getline (cin, mhs[i].alamat.desa);
+        cout << "alamat kota : ";
+        getline (cin, mhs[i].alamat.kota);
+        cout << "umur : ";
+        cin >> mhs[i].umur;
+        cout << endl;
+    }
+cout <<endl;
+
+// menampilkan isi object
+    for (int i = 0; i <= 2; i++)
+    {
+        cout << "nama : " << mhs[i].nama <<endl;
+        cout << "desa : " << mhs[i].alamat.desa <<endl;
+        cout << "kota : " << mhs[i].alamat.kota <<endl;
+        cout << "umur : " << mhs[i].umur <<endl;
+    }
+}
